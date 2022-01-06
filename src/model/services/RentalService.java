1d19e837e8cf88578815941d8491a1827ec23ece
Rafael <rafa.jo.souza@hotmail.com>
2022-01-06 08:52:44 -0300

    Using Interface

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,56 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+
+	private Double pricePerDay;
+	private Double pricePerHour;
+	
+	private TaxService taxService;
+
+	public RentalService() {
+
+	}
+
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		double hours = (double)(t2 - t1) / 1000 / 60 / 60;
+		
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		} else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+
+	public Double getPricePerPay() {
+		return pricePerDay;
+	}
+
+	public void setPricePerPay(Double pricePerPay) {
+		this.pricePerDay = pricePerPay;
+	}
+
+	public Double getPricePerHour() {
+		return pricePerHour;
+	}
+
+	public void setPricePerHour(Double pricePerHour) {
+		this.pricePerHour = pricePerHour;
+	}
+
+}
